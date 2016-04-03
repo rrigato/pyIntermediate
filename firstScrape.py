d409cfed5200fe05bfd956e008f9d6c4faa77d67
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-04-02 17:15:52 -0700

    Update firstScrape.py
    Implemented a functional approach that includes exception handling

--- firstScrape.py
+++ firstScrape.py
@@ -1,15 +1,43 @@
 #in the python standard template library
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-#requests a web page from a directory and prints out the html
-html = urlopen("http://pythonscraping.com/pages/page1.html")
+from urllib.error import HTTPError
+from urllib.error import URLError
+import sys
 
+def getWebpage(website):
+	try:
+		#requests a web page from a directory and prints out the html
+		html = urlopen(website)
+	#catches page not found on server
+	except HTTPError as e:
+		print(e)
+		sys.exit(1)
+	#catches server errors not being found
+	except URLError as e:
+		print(e)
+		print("The server could not be found")
+		sys.exit(1)
+	else:
+		return (html)
+		
+def readTag(html):
+	try:
+		bsObj = BeautifulSoup(html.read())
+		Paragraph = bsObj.di
+	except AttributeError as e:
+		print(e)
+		sys.exit(1)
+	else:
+		return(Paragraph)
+	
 #kaggle leaderboard for bnp paribus
 #html2 = urlopen("https://www.kaggle.com/c/bnp-paribas-cardif-claims-management/leaderboard")
 
-bsObj = BeautifulSoup(html.read())
+html = getWebpage("http://pythonscraping.com/pages/page1.html")
+
 
 #bsObj2 = BeautifulSoup(html.read())
 #prints the header tag
-print(bsObj.h1)
+print(readTag(html))
 
